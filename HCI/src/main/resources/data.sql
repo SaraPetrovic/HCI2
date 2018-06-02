c322e403548674da1150e6610e0596c4676680a7
srbulovicdusan <srbulovicdusan@gmail.com>
2018-06-02 19:27:13 +0200

    inicijalni komit 2

--- HCI/src/main/resources/data.sql
+++ HCI/src/main/resources/data.sql
@@ -1,6 +1,8 @@
 insert into smer(oznaka, naziv, datum, opis) values ('siit', 'softversko inzenjerstvo i informacione tehnologije', '10.10.2014','opis smera' )
 insert into predmet(oznaka, naziv, opis, smer_oznaka, velicina_grupe, duzina_termina, broj_termina, projektor, tabla, pametna_tabla, op_sistem  ) values ('HCI', 'Interakcija covek racunar', 'opis predmeta', 'siit', '15', '2', '4', true, true, false, 'WINDOWS' )
-insert into predmet(oznaka, naziv, opis, smer_oznaka, velicina_grupe, duzina_termina, broj_termina, projektor, tabla, pametna_tabla, op_sistem  ) values ('Asc', 'Baze podataka', 'opis predmeta', 'siit', '15', '2', '4', true, true, false, 'WINDOWS' )
+insert into predmet(oznaka, naziv, opis, smer_oznaka, velicina_grupe, duzina_termina, broj_termina, projektor, tabla, pametna_tabla, op_sistem  ) values ('ISA', 'Baze podataka', 'opis predmeta', 'siit', '15', '3', '4', true, true, false, 'WINDOWS' )
+insert into predmet(oznaka, naziv, opis, smer_oznaka, velicina_grupe, duzina_termina, broj_termina, projektor, tabla, pametna_tabla, op_sistem  ) values ('MRS', 'Baze podataka', 'opis predmeta', 'siit', '15', '4', '4', true, true, false, 'WINDOWS' )
+
 insert into ucionica(oznaka, opis, broj_mesta, projektor, tabla, pametna_tabla, op_sistem ) values ('L1', 'opis ucionice', '25', true, true, true, 'WINLINUX')
 insert into ucionica(oznaka, opis, broj_mesta, projektor, tabla, pametna_tabla, op_sistem ) values ('L2', 'opis ucionice', '25', true, true, true, 'WINLINUX')
 insert into ucionica(oznaka, opis, broj_mesta, projektor, tabla, pametna_tabla, op_sistem ) values ('L3', 'opis ucionice', '25', true, true, true, 'WINLINUX')
